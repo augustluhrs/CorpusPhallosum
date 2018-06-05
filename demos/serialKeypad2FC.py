6829c9adbefdf723c2200b8ce323fdb5126b7c49
DeadAugust <augustluhrs@gmail.com>
2018-06-05 15:06:54 -0700

    keypadTest6-5

--- demos/serialKeypad2FC.py
+++ demos/serialKeypad2FC.py
@@ -11,10 +11,10 @@ client = opc.Client('localhost:7890')
 while 1:
 	read_ser=ser.readline()
 	print(read_ser)
-	#if ser.readline() == 4:
-		#os.system('./python/chase.py')
-	for i in range(numLEDs):
-		pixels = [ (0,0,0) ] * numLEDs
-		pixels[i] = (255,255,255)
-		client.put_pixels(pixels)
-		time.sleep(0.01)
+	if ser.readline() == '4':
+		os.system('./python/chase.py')
+		'''for i in range(numLEDs):
+			pixels = [ (0,0,0) ] * numLEDs
+			pixels[i] = (255,255,255)
+			client.put_pixels(pixels)
+			time.sleep(0.01)'''
